5d7179894ccbc13a8cabd1b4423f920a4bba8460
wickidcow <jaywago@gmail.com>
2026-08-12 00:47:35 -0400

    fix: let ChunkLoader placement handler persist owner data

--- src/main/java/me/gallowsdove/foxymachines/listeners/ChunkLoaderListener.java
+++ src/main/java/me/gallowsdove/foxymachines/listeners/ChunkLoaderListener.java
@@ -5,7 +5,6 @@ import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 import me.gallowsdove.foxymachines.FoxyMachines;
 import me.gallowsdove.foxymachines.Items;
-import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -22,7 +21,7 @@ import javax.annotation.Nonnull;
 public class ChunkLoaderListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onChunkLoaderPlace(@Nonnull BlockPlaceEvent e) {
-        if(e.getBlock().getType() != Material.BEACON) {
+        if (e.getBlock().getType() != Material.BEACON) {
             return;
         }
 
@@ -47,9 +46,9 @@ public class ChunkLoaderListener implements Listener {
         }
 
         NamespacedKey key = new NamespacedKey(FoxyMachines.getInstance(), "chunkloaders");
-
         int i = p.getPersistentDataContainer().getOrDefault(key, PersistentDataType.INTEGER, 0) + 1;
         Config cfg = new Config(FoxyMachines.getInstance());
+
         if (!p.hasPermission("foxymachines.bypass-chunk-loader-limit")) {
             int max = cfg.getInt("max-chunk-loaders");
             if (max != 0 && max < i) {
@@ -58,6 +57,7 @@ public class ChunkLoaderListener implements Listener {
                 return;
             }
         }
+
         int currentComplexity = Slimefun.getGPSNetwork().getNetworkComplexity(p.getUniqueId());
         int requiredComplexity = cfg.getInt("gps-complexity-per-loader") * i;
         if (currentComplexity < requiredComplexity) {
@@ -69,6 +69,5 @@ public class ChunkLoaderListener implements Listener {
         p.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, i);
         b.getChunk().setForceLoaded(true);
         SlimeWorldCompatListener.markManaged(b.getChunk());
-        BlockStorage.addBlockInfo(b, "owner", p.getUniqueId().toString());
     }
 }
